031811de886ccf17bcef9304cff1c284e4a9b818
ES <2662442385@qq.com>
2026-08-18 09:58:27 +0800

    chore: update dep path

--- lua/vv-git/file_policy.lua
+++ lua/vv-git/file_policy.lua
@@ -1,6 +1,6 @@
 -- 文件类型策略：把 vv-git 配置适配为 vv-utils 的内容探测接口
 
-local Fs = require('vv-utils.fs')
+local FileProbe = require('vv-utils.fs.file_probe')
 local M = {}
 
 ---@param path string
@@ -8,7 +8,7 @@ local M = {}
 ---@return VVFsFileInfo?
 function M.binary_info(path, config)
   if not config or not config.intercept then return nil end
-  local info = Fs.inspect_file(path, { extensions = config.extensions })
+  local info = FileProbe.inspect(path, { extensions = config.extensions })
   return info.binary and info or nil
 end
 
